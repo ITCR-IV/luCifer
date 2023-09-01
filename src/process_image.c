4f334ff91bf2532784424798492d260e8b8b81f8
Ignacio Vargas <ignaevc@gmail.com>
2023-09-01 14:06:32 -0600

    Agregar función para clasificar por color

--- src/process_image.c
+++ src/process_image.c
@@ -1,6 +1,49 @@
 #include "process_image.h"
 
-enum DominantColor get_dominant_color(FIBITMAP *img) { return RED; }
+#include <FreeImage.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t max_of_two(size_t x, size_t y) { return (x > y) ? x : y; }
+
+size_t is_max_of_three(size_t check, size_t m, size_t n) {
+  return check > max_of_two(m, n);
+}
+
+enum DominantColor get_dominant_color(FIBITMAP *image) {
+
+  unsigned WIDTH = FreeImage_GetWidth(image);
+  unsigned HEIGHT = FreeImage_GetHeight(image);
+
+  RGBQUAD color;
+
+  size_t red_count = 0;
+  size_t blue_count = 0;
+  size_t green_count = 0;
+
+  for (unsigned i = 0; i < WIDTH; i++) {
+    for (unsigned j = 0; j < HEIGHT; j++) {
+      if (FreeImage_GetPixelColor(image, i, j, &color)) {
+        red_count += color.rgbRed;
+        blue_count += color.rgbBlue;
+        green_count += color.rgbGreen;
+      }
+    }
+  }
+
+  if ((red_count >= blue_count) && (red_count >= green_count))
+    return RED;
+
+  if ((blue_count >= red_count) && (blue_count >= green_count))
+    return BLUE;
+
+  if ((green_count >= red_count) && (green_count >= red_count))
+    return GREEN;
+
+  // Unreachable
+  fprintf(stderr, "Unreachable reached!");
+  exit(5);
+}
 
 void equalize_histogram(FIBITMAP *img) {}
 
@@ -14,5 +57,5 @@ const char *get_dominant_color_subdir(enum DominantColor color) {
     return BLUE_COLOR_DIR;
     break;
   }
-	return "INVALID_COLOR";
+  return "INVALID_COLOR";
 }
